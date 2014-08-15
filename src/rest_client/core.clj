8201d8e4f163657a80dde2a8744f7f3bfed4d101
dzito <dan.zito@adroll.com>
2014-08-15 15:30:09 -0700

    stub functions to create and delete orders

--- src/rest_client/core.clj
+++ src/rest_client/core.clj
@@ -11,9 +11,25 @@
 
 (defn get-order
   "Get a single order by ID"
-  ([id]
-   (get-order const/base-url id))
-  ([url id]
-   ((http/get (str url "/order/" id)) :body)))
+  ([order-id]
+   (get-order const/base-url order-id))
+  ([base-url order-id]
+   ((http/get (str base-url "/order/" order-id)) :body)))
+
+(defn create-order
+  "Create a new order. TODO: json-ify the order"
+  ([order]
+    (create-order const/base-url order-content))
+  ([base-url order]
+   ((http/post (str base-url "/order")
+               {:body (str order)
+                :content-type :json}))))
+
+(defn delete-order
+  "Delete a completed order."
+  ([order-id]
+   (delete-order const/base-url order-id))
+  ([base-url order-id]
+   ((http/delete (str base-url "/order/" order-id))))
 
 (defn -main [& args])
