d93f258ba9d75832e691e9d632ee59e85285c2c0
Duncan McGreggor <duncan@cogitat.io>
2014-08-15 14:23:56 -0700

    Updated deps.

--- project.clj
+++ project.clj
@@ -3,4 +3,10 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]])
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [org.clojure/data.json "0.2.5"]
+                 [clj-http "1.0.0"]]
+  :plugins [[lein-git-deps "0.0.2-SNAPSHOT"]]
+  :main rest-client.core
+  :git-dependencies [
+    ["https://github.com/SemanticSugar/hack-day-clj-rest-server.git"]])
