a131e1675196464d22afc3479db09ef114256ef8
Duncan McGreggor <duncan@cogitat.io>
2014-08-15 08:43:20 -0700

    Initial commit.

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to rest-client
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ test/rest_client/core_test.clj
@@ -0,0 +1,7 @@
+(ns rest-client.core-test
+  (:require [clojure.test :refer :all]
+            [rest-client.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
